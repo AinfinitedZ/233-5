d83368eafb3d5e46e65e6c85f72552262ac7f549
AinfinitedZ <43518696+AinfinitedZ@users.noreply.github.com>
2022-04-14 15:10:12 -0400

    approach 91

--- src/App.java
+++ src/App.java
@@ -1,12 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        for(int i = 0; i < 100; i++){
-            int[] randomArray = Sort.randomArray(10, 1, 10000);
-            long startTime = System.nanoTime();
-            Sort.insertSort(randomArray);
-            long endTime = System.nanoTime();
-            long difference = endTime - startTime;
-            System.out.println(difference);
-        }
+        int[] randomArray = Sort.randomArray(500, 1, 10000);
+        long startTime = System.nanoTime();
+        Sort.insertSort(randomArray);
+        long endTime = System.nanoTime();
+        long difference = endTime - startTime;
+        System.out.println(difference);
+        
     }
 }
